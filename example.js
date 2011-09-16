9109d2e84397ed3b569819c4014865c5a8732458
Morgan "ARR!" Allen <morganrallen@gmail.com>
2011-09-16 08:50:37 -0700

    breaks rainbow into sequencer and map function
    this adds the ability to easily add new sequences. Adds two examples (zebra, red,white,blue). Also exports the addSequencer method to programs including colors can add new sequences on the fly

--- example.js
+++ example.js
@@ -1,20 +1,30 @@
-var sys = require('sys');
 var colors = require('./colors');
 
 //colors.mode = "browser";
 
 var test = colors.red("hopefully colorless output");
-sys.puts('Rainbows are fun!'.rainbow);
-sys.puts('So '.italic + 'are'.underline + ' styles! '.bold + 'inverse'.inverse); // styles not widely supported
-sys.puts('Chains are also cool.'.bold.italic.underline.red); // styles not widely supported
-//sys.puts('zalgo time!'.zalgo);
-sys.puts(test.stripColors);
-sys.puts("a".grey + " b".black);
+console.log('Rainbows are fun!'.rainbow);
+console.log('So '.italic + 'are'.underline + ' styles! '.bold + 'inverse'.inverse); // styles not widely supported
+console.log('Chains are also cool.'.bold.italic.underline.red); // styles not widely supported
+//console.log('zalgo time!'.zalgo);
+console.log(test.stripColors);
+console.log("a".grey + " b".black);
 
-sys.puts(colors.rainbow('Rainbows are fun!'));
-sys.puts(colors.italic('So ') + colors.underline('are') + colors.bold(' styles! ') + colors.inverse('inverse')); // styles not widely supported
-sys.puts(colors.bold(colors.italic(colors.underline(colors.red('Chains are also cool.'))))); // styles not widely supported
-//sys.puts(colors.zalgo('zalgo time!'));
-sys.puts(colors.stripColors(test));
-sys.puts(colors.grey("a") + colors.black(" b"));
+console.log("Zebras are so fun!".zebra);
 
+console.log(colors.rainbow('Rainbows are fun!'));
+console.log(colors.italic('So ') + colors.underline('are') + colors.bold(' styles! ') + colors.inverse('inverse')); // styles not widely supported
+console.log(colors.bold(colors.italic(colors.underline(colors.red('Chains are also cool.'))))); // styles not widely supported
+//console.log(colors.zalgo('zalgo time!'));
+console.log(colors.stripColors(test));
+console.log(colors.grey("a") + colors.black(" b"));
+
+colors.addSequencer("america", function(letter, i, exploded) {
+	switch(i%3) {
+		case 0: return letter.red;
+		case 1: return letter.white;
+		case 2: return letter.blue;
+	}
+});
+
+console.log("AMERICA! F--K YEAH!".america);
