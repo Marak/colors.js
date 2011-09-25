d1eab2c1dce18ebf430ae7f9c9047117f72d52f7
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-09-25 10:11:37 +0200

    [minor] s/sys/util/g in example.js
    Requiring `sys` emits a warning since joyent/node@8d70cc6.

--- example.js
+++ example.js
@@ -1,20 +1,20 @@
-var sys = require('sys');
+var util = require('util');
 var colors = require('./colors');
 
 //colors.mode = "browser";
 
 var test = colors.red("hopefully colorless output");
-sys.puts('Rainbows are fun!'.rainbow);
-sys.puts('So '.italic + 'are'.underline + ' styles! '.bold + 'inverse'.inverse); // styles not widely supported
-sys.puts('Chains are also cool.'.bold.italic.underline.red); // styles not widely supported
-//sys.puts('zalgo time!'.zalgo);
-sys.puts(test.stripColors);
-sys.puts("a".grey + " b".black);
+util.puts('Rainbows are fun!'.rainbow);
+util.puts('So '.italic + 'are'.underline + ' styles! '.bold + 'inverse'.inverse); // styles not widely supported
+util.puts('Chains are also cool.'.bold.italic.underline.red); // styles not widely supported
+//util.puts('zalgo time!'.zalgo);
+util.puts(test.stripColors);
+util.puts("a".grey + " b".black);
 
-sys.puts(colors.rainbow('Rainbows are fun!'));
-sys.puts(colors.italic('So ') + colors.underline('are') + colors.bold(' styles! ') + colors.inverse('inverse')); // styles not widely supported
-sys.puts(colors.bold(colors.italic(colors.underline(colors.red('Chains are also cool.'))))); // styles not widely supported
-//sys.puts(colors.zalgo('zalgo time!'));
-sys.puts(colors.stripColors(test));
-sys.puts(colors.grey("a") + colors.black(" b"));
+util.puts(colors.rainbow('Rainbows are fun!'));
+util.puts(colors.italic('So ') + colors.underline('are') + colors.bold(' styles! ') + colors.inverse('inverse')); // styles not widely supported
+util.puts(colors.bold(colors.italic(colors.underline(colors.red('Chains are also cool.'))))); // styles not widely supported
+//util.puts(colors.zalgo('zalgo time!'));
+util.puts(colors.stripColors(test));
+util.puts(colors.grey("a") + colors.black(" b"));
 
